27b6b8366790455b389e88bbe91963f3398b0a8b
Sujin Yun <yuntnwls8260@gmail.com>
2021-05-04 21:01:09 +0900

    백준 1461 버그 수정

--- src/com/backjoon/greedy/t1461/Main.java
+++ src/com/backjoon/greedy/t1461/Main.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static int answer = 0;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,12 +27,11 @@ public class Main {
 				pLoc.add(loc);
 			}
 		}
-		int answer = solution(n, m, mLoc, pLoc);
+		solution(n, m, mLoc, pLoc);
 		System.out.println(answer);
 	}
 
 	private static int solution(int n, int m, List<Integer> mLoc, List<Integer> pLoc) {
-		int answer = 0;
 		// 오름차순로 정렬 
 		Collections.sort(mLoc, new Comparator<Integer>() {
 			@Override
@@ -45,35 +46,43 @@ public class Main {
 			}
 		});
 		
-		int idx = 0;
 		int myLoc = 0;
-		// 오른쪽의 책들이 모두 정리될 때까지 
-		int pSize = pLoc.size();
-		int mSize = mLoc.size();
-		while (idx < pSize) {
-			if (pLoc.get(idx)> myLoc) {
-				// 가장 먼 곳 
-				answer += pLoc.get(idx);
-				myLoc += pLoc.get(idx);
+		if (pLoc.size() > 0 && mLoc.size() > 0) {
+			// 음수와 양수가 모두 있는 경우는 순서가 중요
+			int pMax = pLoc.get(0);
+			int mMax = mLoc.get(0);
+			if (pMax > mMax) {
+				myLoc = move(mLoc, myLoc, m);
+				move(pLoc, myLoc, m);
 			} else {
-				answer += (pLoc.get(idx)*2);
+				myLoc = move(pLoc, myLoc, m);
+				move(mLoc, myLoc, m);
 			}
-			idx += m;
-		}
-		idx = 0;
-		// 왼쪽 책들이 모두 정리될 때 
-		while (idx < mSize) {
-			if (mLoc.get(idx)> myLoc) {
-				// 가장 먼 곳 
-				answer += mLoc.get(idx);
-				answer += myLoc;	// +와-가 모두 있는 경우 +가 끝나고 -를 갈떄 원점을 한번은 지나야함  
-				myLoc += mLoc.get(idx);
-			} else {
-				answer += (mLoc.get(idx)*2);
-			}
-			idx += m;
+			// 음수에서 양수/양수에서 음수로 가는 경우 
+			// 무조건 한번은 원점을 지나므로 최대 값이 더 작은 수를 더해줌 
+			answer += myLoc;
+		} else if (pLoc.size() > 0) {
+			move(pLoc, myLoc, m);
+		} else {
+			move(mLoc, myLoc, m);
 		}
 		return answer;
+	}
+	
+	private static int move(List<Integer> loc, int currentLoc, int m) {
+		int idx = 0;
+		int locSize = loc.size();
+		while (idx < locSize) {
+			if (loc.get(idx)> currentLoc) {
+				// 가장 먼 곳 
+				answer += loc.get(idx);
+				currentLoc += loc.get(idx);
+			} else {
+				answer += (loc.get(idx)*2);
+			}
+			idx += m;
+		}
+		return currentLoc;
 	}
 }
 
