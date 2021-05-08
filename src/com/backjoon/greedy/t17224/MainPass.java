754491ba16d38d8b3826dc403c1c4db1902269a0
Sujin Yun <yuntnwls8260@gmail.com>
2021-05-08 13:16:15 +0900

    백준 17724 pass

--- src/com/backjoon/greedy/t17224/MainPass.java
+++ src/com/backjoon/greedy/t17224/MainPass.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class MainPass {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -12,29 +12,36 @@ public class Main {
 		int l = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 		int[][] level = new int [n][2];
+		int hardCnt = 0;
+		int easyCnt = 0;
 		for (int i=0; i<n; i++) {
 			st = new StringTokenizer(br.readLine());
 			level[i][0] = Integer.parseInt(st.nextToken());
 			level[i][1] = Integer.parseInt(st.nextToken());
+			if (level[i][1] <= l) {
+				hardCnt++;
+			} else if (level[i][0] <=l) {
+				easyCnt++;
+			}
 		}
-		int answer = solution(n, l, k, level);
+		int answer = solution(k, hardCnt, easyCnt);
 		System.out.println(answer);
 	}
 
-	private static int solution(int n, int l, int k, int[][] level) {
+	private static int solution(int k, int hardCnt, int easyCnt) {
 		int answer = 0;
-		
-		for (int i=0; i<n; i++) {
-			if (i >= k ) {
-				break;
+		// 문제를 순서대로 풀지 않아도 됨 
+		// 먼저 가능한 어려운 문제부터 풀기 
+		if (hardCnt > k) {
+			answer = k*140;
+		} else {
+			answer = hardCnt*140;
+			k-=hardCnt;
+			if (easyCnt > k) {
+				answer += k*100;
+			} else {
+				answer += easyCnt * 100;
 			}
-			
-			if (level[i][0] <= l) {
-				answer += 100;
-			}
-			if (level[i][1] <= l) {
-				answer += 40;
-			} 
 		}
 		return answer;
 	}
